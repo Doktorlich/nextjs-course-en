b30480d5aa05e7aca10bc2ac9f0d38bb64906b33
Doktorlich <sergei199522@mail.ru>
2026-06-15 15:13:14 +0300

    feat: module 05/03

--- 05-data-fetching-deep-dive/01-starting-project/app/(content)/layout.js
+++ 05-data-fetching-deep-dive/01-starting-project/app/(content)/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
             <body>
                 <div id={"page"}>
                     <MainHeader />
+
                     {children}
                 </div>
             </body>
